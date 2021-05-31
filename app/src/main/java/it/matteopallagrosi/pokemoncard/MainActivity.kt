50edbc698afa13aac74e96be999750d82b8dbb8c
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2021-05-31 13:31:38 +0200

    prova 2

--- app/src/main/java/it/matteopallagrosi/pokemoncard/MainActivity.kt
+++ app/src/main/java/it/matteopallagrosi/pokemoncard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     val hello: String = "ciao"
+    val string: String = "come stai"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
