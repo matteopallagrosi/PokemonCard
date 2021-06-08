c1ead76c60e7ae23fe4607c5a3ce4ebeb49fe4e3
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2021-06-08 17:02:13 +0200

    prova

--- app/src/main/java/it/fasm/pokemoncard/MainActivity.kt
+++ app/src/main/java/it/fasm/pokemoncard/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
 
-    private var response = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
