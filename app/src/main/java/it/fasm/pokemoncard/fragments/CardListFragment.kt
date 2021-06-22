df8596f5db9d775d61104176da9eae2018214a39
checocugio <alessio.vinci00@libero.it>
2021-06-22 19:13:29 +0200

    ciao

--- app/src/main/java/it/fasm/pokemoncard/fragments/CardListFragment.kt
+++ app/src/main/java/it/fasm/pokemoncard/fragments/CardListFragment.kt
@@ -1,38 +1,59 @@
 package it.fasm.pokemoncard.fragments
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.RetryPolicy
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import it.fasm.pokemoncard.R
 import it.fasm.pokemoncard.adapters.CardsAdapter
 import it.fasm.pokemoncard.databinding.FragmentCardListBinding
-
 import it.fasm.pokemoncard.model.Card
+import org.json.JSONObject
 
 class CardListFragment : Fragment() {
 
     private lateinit var binding: FragmentCardListBinding
     private lateinit var adapter: CardsAdapter
     private var cards = ArrayList<Card>()
-    private var cardImages = HashMap<String , Bitmap>()
+    private var cardImages = HashMap<String, Bitmap>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+
+        super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentCardListBinding.inflate(layoutInflater, container, false)
-        return binding.root
 
-        val set = intent.extras?.getString("set")
+        val view = binding.root
 
-        println(set)
+        //val set = intent.extras.getString("set")
+
+        // NON CREDO FUNZIONI
+        val bundle = Bundle()
+        bundle.putString("set", "From MainActivity")
+        //set Fragmentclass Arguments
+        val fragobj = CardListFragment()
+        fragobj.arguments = bundle
+
 
         class GridSpacingItemDecoration(
             private val spanCount: Int,
@@ -72,8 +93,8 @@ class CardListFragment : Fragment() {
         }
 
 
-        binding.rvCards.layoutManager = GridLayoutManager(this, 3)
-        adapter = CardsAdapter(cards, cardImages, this)
+        binding.rvCards.layoutManager = GridLayoutManager(context, 3)
+        adapter = CardsAdapter(cards, cardImages, requireContext())
         binding.rvCards.adapter = adapter
 
         val spanCount = 3 // 3 columns
@@ -81,11 +102,87 @@ class CardListFragment : Fragment() {
         val spacing = 20 // 50px
 
         val includeEdge = true
-        binding.rvCards.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+        binding.rvCards.addItemDecoration(
+            GridSpacingItemDecoration(
+                spanCount,
+                spacing,
+                includeEdge
+            )
+        )
 
-        setUICard(set)
+        setUICard(bundle.toString())
+        return view
     }
+    fun setUICard(set: String?) {
+        var url = "https://api.pokemontcg.io/v2/cards?q=set.id:$set"
 
+        val queue = Volley.newRequestQueue(context)
+
+
+        val jsonObjectRequest = object : StringRequest(
+            Request.Method.GET, url,
+            Response.Listener { response ->
+
+                var jo = JSONObject(response)
+                var ja = jo.getJSONArray("data")
+                println(ja.toString())
+
+                var gson = Gson()
+
+                val sType = object : TypeToken<ArrayList<Card>>() {}.type
+
+                cards.addAll(gson.fromJson<ArrayList<Card>>(ja.toString(), sType))
+                val cardBack = BitmapFactory.decodeResource(this.resources, R.drawable.card_back)
+                for (card in cards) {
+                    cardImages[card.id] = cardBack
+                }
+                adapter.notifyDataSetChanged()
+                val requestQueue = Volley.newRequestQueue(context)
+                for (card in cards) {
+                    val imageRequest = ImageRequest(card.images.large, {
+                        cardImages[card.id] = it
+                        println("OK")
+                        adapter.notifyDataSetChanged()
+                        binding.tvnumbercards.text = cards.size.toString()
+                        binding.tvdate.text = cards[0].set.releaseDate.toString()
+
+                    }, 0, 0,
+                        ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
+                        { error ->
+                            Log.e("Volley", error.toString())
+                        })
+                    requestQueue.add(imageRequest)
+                }
+            },
+            Response.ErrorListener { error ->
+                Log.e("Volley", error.toString())
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["X-Api-Key"] = "99967d70-c1ae-4dcb-a297-6d613706472d\n"
+                return headers
+            }
+        }
+
+        jsonObjectRequest.setRetryPolicy(object : RetryPolicy {
+            override fun getCurrentTimeout(): Int {
+                return 50000
+            }
+
+            override fun getCurrentRetryCount(): Int {
+                return 50000
+            }
+
+            override fun retry(error: VolleyError?) {
+            }
+
+
+        })
+
+        queue.add(jsonObjectRequest)
+
+    }
 
 
 }
--- app/src/main/java/it/fasm/pokemoncard/fragments/SetsFragment.kt
+++ app/src/main/java/it/fasm/pokemoncard/fragments/SetsFragment.kt
@@ -1,13 +1,14 @@
-package it.fasm.pokemoncard
+package it.fasm.pokemoncard.fragments
 
-import android.R.id.message
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
@@ -16,53 +17,62 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import it.fasm.pokemoncard.MainActivity
 import it.fasm.pokemoncard.adapters.SetsAdapter
 import it.fasm.pokemoncard.databinding.ActivitySetsBinding
+import it.fasm.pokemoncard.databinding.FragmentSetsBinding
 import it.fasm.pokemoncard.model.CardSet
 import org.json.JSONObject
 
+class SetsFragment : Fragment() {
 
-class SetsActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySetsBinding
+    private lateinit var binding: FragmentSetsBinding
     private lateinit var adapter: SetsAdapter
     private var sets = ArrayList<CardSet>()
     private var logos = HashMap<String, Bitmap>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
         super.onCreate(savedInstanceState)
-        binding = ActivitySetsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = FragmentSetsBinding.inflate(layoutInflater)
+
+        val view = binding.root
 
 
-        val serie = intent.extras?.getString("serie")
-        //
-        //val intent = Intent(
-        //getActivity(this,).getBaseContext(),
-        //MainActivity::class.java
-        //)
-        //intent.putExtra("serie", message)
-        //getActivity().startActivity(intent)
-        //
+        //val serie = intent.extras?.getString("serie")
 
-        binding.rvSets.layoutManager = GridLayoutManager(this, 2)
-        adapter = SetsAdapter(sets, logos, this)
+        val intent = Intent(
+            getActivity()?.getBaseContext(),
+        MainActivity::class.java
+        )
+        intent.putExtra("serie", Bundle())
+        getActivity()?.startActivity(intent)
+
+
+        binding.rvSets.layoutManager = GridLayoutManager(context, 2)
+        adapter = SetsAdapter(sets, logos, requireContext())
         binding.rvSets.adapter = adapter
 
-        setUI(serie)
+        setUI(bundle.toString())
 
 
         //binding.rvSets.layoutManager = GridLayoutManager(this, 2)
         //binding.rvSets.adapter = SetsAdapter()
+        return view
     }
 
     fun setUI(serie: String?) {
         var url = "https://api.pokemontcg.io/v2/sets?q=series:$serie"
 
-        val queue = Volley.newRequestQueue(this)
+        val queue = Volley.newRequestQueue(context)
 
 
-        val jsonObjectRequest = object : StringRequest(Request.Method.GET, url,
+        val jsonObjectRequest = object : StringRequest(
+            Request.Method.GET, url,
             Response.Listener { response ->
 
                 var jo = JSONObject(response)
@@ -74,7 +84,7 @@ class SetsActivity : AppCompatActivity() {
                 val sType = object : TypeToken<ArrayList<CardSet>>() {}.type
 
                 sets.addAll(gson.fromJson<ArrayList<CardSet>>(ja.toString(), sType))
-                val requestQueue = Volley.newRequestQueue(this)
+                val requestQueue = Volley.newRequestQueue(context)
                 for (set in sets) {
                     val imageRequest = ImageRequest(set.images.logo, {
                         logos[set.id] = it
@@ -103,6 +113,5 @@ class SetsActivity : AppCompatActivity() {
         queue.add(jsonObjectRequest)
     }
 
-
-
+}
 }
