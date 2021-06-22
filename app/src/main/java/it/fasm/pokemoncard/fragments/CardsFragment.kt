0f278c0615449af942b19a3058048093ff4b28ef
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2021-06-22 18:27:15 +0200

    prova

--- app/src/main/java/it/fasm/pokemoncard/fragments/CardsFragment.kt
+++ app/src/main/java/it/fasm/pokemoncard/fragments/CardsFragment.kt
@@ -7,8 +7,11 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.ViewCompat.canScrollHorizontally
+import androidx.core.view.children
+import androidx.core.view.forEach
 import androidx.core.view.iterator
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.fasm.pokemoncard.R
 import it.fasm.pokemoncard.adapters.SeriesAdapter
 import it.fasm.pokemoncard.databinding.FragmentCardsBinding
@@ -38,19 +41,30 @@ class CardsFragment : Fragment() {
         _binding = FragmentCardsBinding.inflate(inflater, container, false)
         val view = binding.root
         binding.scrollCards.isHorizontalScrollBarEnabled = false
+        var state = false
+        binding.rvSeries.addOnItemTouchListener(object : RecyclerView.SimpleOnItemTouchListener() {
+            override fun onInterceptTouchEvent(view: RecyclerView, e: MotionEvent): Boolean {
+                return state
+            }
+        })
 
-        binding.imageView.setOnClickListener(){
-            binding.ivcardforeground.setImageResource(R.drawable.pokemon1)
-            binding.ivcardforeground.visibility = View.VISIBLE
-            binding.floatingActionButton3.visibility = View.VISIBLE
+        var i = 1
+        binding.layout.children.forEach {
+            val id = resources.getIdentifier("pokemon$i", "drawable", context?.packageName)
+            it.setOnClickListener(){
+                //var d = ContextCompat.getDrawable(requireContext(),id)
+                binding.ivcardforeground.setImageResource(id)
+                binding.ivcardforeground.visibility = View.VISIBLE
+                binding.floatingActionButton3.visibility = View.VISIBLE
+                state = true
+            }
+            i++
         }
-
-
-
 
         binding.floatingActionButton3.setOnClickListener(){
             binding.floatingActionButton3.visibility  =View.INVISIBLE
             binding.ivcardforeground.visibility = View.INVISIBLE
+            state = false
         }
 
         val adapter = SeriesAdapter(requireContext())
